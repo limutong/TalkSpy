7cea94c11855ac2d50670b43c37c26fc41144bb5
limutong <mutongli@gmail.com>
2016-12-21 00:37:42 +0800

    ts-003 for the function version, story main page v1 ok

--- TSWeChat/Classes/Discover/TSDiscoverViewController.swift
+++ TSWeChat/Classes/Discover/TSDiscoverViewController.swift
@@ -2,32 +2,28 @@
 //  TSDiscoverViewController.swift
 //  TSWeChat
 //
-//  Created by Hilen on 1/8/16.
-//  Copyright © 2016 Hilen. All rights reserved.
+//  Created by  Hilen on 1/8/16.
+//  Modified by MT
 //
 
 import UIKit
 
 class TSDiscoverViewController: UIViewController {
     @IBOutlet weak var listTableView: UITableView!
+    //ts-003 For the function version , story
     fileprivate let itemDataSouce: [[(name: String, iconImage: UIImage)]] = [
         [
-            ("朋友圈", TSAsset.Ff_IconShowAlbum.image),
+            ("回忆篇", TSAsset.Ff_IconShowAlbum.image),
+            ("现在篇", TSAsset.MoreGame.image),
+            ("N的调查报告", TSAsset.Ff_IconQRCode.image),
         ],
         [
-            ("扫一扫", TSAsset.Ff_IconQRCode.image),
-            ("摇一摇", TSAsset.Ff_IconShake.image),
-        ],
-        [
-            ("附近的人", TSAsset.Ff_IconLocationService.image),
-            ("漂流瓶", TSAsset.Ff_IconBottle.image),
-        ],
-        [
-//            ("购物", TSAsset.Icon_bustime.image),
-            ("游戏", TSAsset.MoreGame.image),
+            ("每个人的故事", TSAsset.Ff_IconBottle.image),
         ],
     ]
+    //ts-003 For the function version , story end
     override func viewDidLoad() {
+        print(" -------- 1 ------ ")
         super.viewDidLoad()
         //ts-001 story
         self.title = "故事"
@@ -64,6 +60,7 @@ class TSDiscoverViewController: UIViewController {
 // MARK: @protocol - UITableViewDelegate
 extension TSDiscoverViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+                print(" -------- 3 ------ ")
         if section == 0 {
             return 15
         } else {
@@ -72,30 +69,48 @@ extension TSDiscoverViewController: UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        print(" -------- 3.1 ------ ")
         return 0.00001
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(" -------- 3.2 ------ ")
         tableView.deselectRow(at: indexPath, animated: true)
+        print(" chicked indexPath : ",indexPath)
+
+        //init view controller from nib
+        let controllerX = TSContactsViewController.ts_initFromNib()  //通讯录
+        controllerX.tabBarItem!.title = "Story Details"  //set title
+        controllerX.tabBarItem!.image = TSAsset.Tabbar_mainframe.image //set image
+        controllerX.tabBarItem!.selectedImage = TSAsset.Tabbar_mainframeHL.image.withRenderingMode(.alwaysOriginal)     //set image
+        controllerX.tabBarItem!.setTitleTextAttributes([NSForegroundColorAttributeName: UIColor.lightGray], for: UIControlState())
+        controllerX.tabBarItem!.setTitleTextAttributes([NSForegroundColorAttributeName: UIColor.tabbarSelectedTextColor], for: .selected)
+        
+        //fire
+        navigationController?.pushViewController(controllerX, animated: true)
     }
 }
 
 // MARK: @protocol - UITableViewDataSource
 extension TSDiscoverViewController: UITableViewDataSource {
-    func numberOfSections(in tableView: UITableView) -> Int {
+    func numberOfSections(in tableView: UITableView) -> Int {   //define rows
+        print(" -------- 4.1 ------ ")
         return self.itemDataSouce.count
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print(" -------- 4.2 ------ ")
         let rows = self.itemDataSouce[section]
         return rows.count
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        print(" -------- 4.3 ------ ")
         return self.listTableView.estimatedRowHeight
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print(" -------- 4.4 ------ ")
         let cell :TSImageTextTableViewCell = tableView.ts_dequeueReusableCell(TSImageTextTableViewCell.self)
         let item = self.itemDataSouce[indexPath.section][indexPath.row]
         cell.iconImageView.image = item.iconImage
