5f9e39b9287cf8c0b9bc9750499776d68c52293e
limutong <mutongli@gmail.com>
2016-12-21 12:37:31 +0800

    tsp-003 Create POC story details uitableview

--- TSWeChat/Classes/TSStoryViewController.swift
+++ TSWeChat/Classes/TSStoryViewController.swift
@@ -1,40 +1,40 @@
 //
-//  TSDiscoverViewController.swift
+//  TSStoryViewController.swift
 //  TSWeChat
 //
-//  Created by  Hilen on 1/8/16.
-//  Modified by MT
+//  Created by  MT on 20/Dec/16.
 //
 
 import UIKit
 
-class TSDiscoverViewController: UIViewController {
+class TSStoryViewController: UIViewController {
+    
     @IBOutlet weak var listTableView: UITableView!
     //ts-003 For the function version , story
     fileprivate let itemDataSouce: [[(name: String, iconImage: UIImage)]] = [
         [
-            ("回忆篇", TSAsset.Ff_IconShowAlbum.image),
-            ("现在篇", TSAsset.MoreGame.image),
-            ("N的调查报告", TSAsset.Ff_IconQRCode.image),
-        ],
+            ("xxx", TSAsset.Ff_IconShowAlbum.image),
+            ("yyy", TSAsset.MoreGame.image),
+            ("zzz", TSAsset.Ff_IconQRCode.image),
+            ],
         [
-            ("每个人的故事", TSAsset.Ff_IconBottle.image),
-        ],
-    ]
+            ("ttt", TSAsset.Ff_IconBottle.image),
+            ],
+        ]
     //ts-003 For the function version , story end
     override func viewDidLoad() {
         print(" -------- 1 ------ ")
         super.viewDidLoad()
         //ts-001 story
-        self.title = "故事"
+        self.title = "Story Details"
         self.view.backgroundColor = UIColor.viewBackgroundColor
         self.listTableView.ts_registerCellNib(TSImageTextTableViewCell.self)
         self.listTableView.estimatedRowHeight = 44
         self.listTableView.tableFooterView = UIView()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     deinit {
         log.verbose("deinit")
     }
@@ -44,23 +44,23 @@ class TSDiscoverViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
 
 // MARK: @protocol - UITableViewDelegate
-extension TSDiscoverViewController: UITableViewDelegate {
+extension TSStoryViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-                print(" -------- 3 ------ ")
+        print(" -------- 3 ------ ")
         if section == 0 {
             return 15
         } else {
@@ -77,22 +77,22 @@ extension TSDiscoverViewController: UITableViewDelegate {
         print(" -------- 3.2 ------ ")
         tableView.deselectRow(at: indexPath, animated: true)
         print(" chicked indexPath : ",indexPath)
-
         
-        let controllerY = TSStoryViewController.ts_initFromNib()  //Story new page
-        controllerY.tabBarItem!.title = "Story Details"  //set title
-        controllerY.tabBarItem!.image = TSAsset.Tabbar_mainframe.image //set image
-        controllerY.tabBarItem!.selectedImage = TSAsset.Tabbar_mainframeHL.image.withRenderingMode(.alwaysOriginal)     //set image
-        controllerY.tabBarItem!.setTitleTextAttributes([NSForegroundColorAttributeName: UIColor.lightGray], for: UIControlState())
-        controllerY.tabBarItem!.setTitleTextAttributes([NSForegroundColorAttributeName: UIColor.tabbarSelectedTextColor], for: .selected)
+        //init from a inb with same name of the class, return UIViewController
+        let controllerX = TSContactsViewController.ts_initFromNib()  //通讯录
+        controllerX.tabBarItem!.title = "Story Details"  //set title
+        controllerX.tabBarItem!.image = TSAsset.Tabbar_mainframe.image //set image
+        controllerX.tabBarItem!.selectedImage = TSAsset.Tabbar_mainframeHL.image.withRenderingMode(.alwaysOriginal)     //set image
+        controllerX.tabBarItem!.setTitleTextAttributes([NSForegroundColorAttributeName: UIColor.lightGray], for: UIControlState())
+        controllerX.tabBarItem!.setTitleTextAttributes([NSForegroundColorAttributeName: UIColor.tabbarSelectedTextColor], for: .selected)
         
         //fire
-        navigationController?.pushViewController(controllerY, animated: true)
+        navigationController?.pushViewController(controllerX, animated: true)
     }
 }
 
 // MARK: @protocol - UITableViewDataSource
-extension TSDiscoverViewController: UITableViewDataSource {
+extension TSStoryViewController: UITableViewDataSource {
     func numberOfSections(in tableView: UITableView) -> Int {   //define rows
         print(" -------- 4.1 ------ ")
         return self.itemDataSouce.count
